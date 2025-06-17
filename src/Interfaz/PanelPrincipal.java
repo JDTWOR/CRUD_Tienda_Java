ab5f0180f7dbbec23e2ea9d015e3963a4564bf57
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-06-16 20:57:33 -0500

    base Tienda JAVA

---
+++ src/Interfaz/PanelPrincipal.java
@@ -0,0 +1,9 @@
+package src.Interfaz;
+
+import java.awt.BorderLayout;
+
+/**Ventana principal de la aplicación */
+public class PanelPrincipal extends JFrame {
+	/** constante para la conexión a la base de datos */
+    public static final String DB_URL = "jdbc:sqlite:tiendas.db";
+}
