f48d2861c8123e8c1215d558fdeb8e9535cdad6a
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-06-19 10:47:59 -0500

    Contruccion de la clase tienda

--- src/Mundo/Tienda.java
+++ src/Mundo/Tienda.java
@@ -1,5 +1,14 @@
 package Mundo;
 
 public class Tienda {
-    
+  private String nombre;
+  private String direccion;
+  private String contacto;
+
+  public Tienda(String pnombre, String pdireccion, String pcontacto) {
+    nombre = pnombre;
+    direccion = pdireccion;
+    contacto = pcontacto;
+  }
+
 }
